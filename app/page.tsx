f9799f49eee648914b3658cdfef1862d666d7697
luizernane127 <luizernane127@gmail.com>
2025-09-05 15:10:40 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 "use client";
+import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
